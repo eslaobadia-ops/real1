39fb965d2c5992a6538743a945da7a593efc3764
eslaobadia-ops <eslaobadia@gmail.com>
2025-12-17 17:15:43 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,30 @@
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
 
 export default function Login() {
-  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const { login } = useAuth();
 
-  function handleLogin(e) {
-    e.preventDefault();
-    const email = e.target.email.value;
-
-    if (email.includes("admin")) {
-      navigate("/admin");
-    } else {
-      navigate("/dashboard");
-    }
+  function handleLogin() {
+    if (!email) return alert("Enter email");
+    login(email);
   }
 
   return (
-    <div style={{ padding: 40 }}>
+    <div style={{ padding: 60 }}>
       <h1>Jaycrest Group of School</h1>
-      <form onSubmit={handleLogin}>
-        <input name="email" placeholder="Email" required />
-        <br /><br />
-        <button type="submit">Login</button>
-      </form>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        style={{ padding: 10, width: 300 }}
+      />
+
+      <br /><br />
+
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }

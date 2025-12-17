81d6cd94812c29c24b3248780c53d0ed355b205b
eslaobadia-ops <eslaobadia@gmail.com>
2025-12-17 18:04:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Results from "./pages/Results";
 import Payments from "./pages/Payments";
+import Admin from "./pages/Admin";
+import Layout from "./layout/Layout";
 import RequireAuth from "./components/RequireAuth";
 import RequirePayment from "./components/RequirePayment";
 
@@ -14,34 +16,26 @@ export default function App() {
         {/* PUBLIC */}
         <Route path="/" element={<Login />} />
 
-        {/* AUTH ONLY */}
+        {/* AUTHENTICATED AREA */}
         <Route
-          path="/dashboard"
           element={
             <RequireAuth>
-              <Dashboard />
+              <Layout />
             </RequireAuth>
           }
-        />
-
-        {/* AUTH + PAYMENT */}
-        <Route
-          path="/results"
-          element={
-            <RequirePayment>
-              <Results />
-            </RequirePayment>
-          }
-        />
-
-        <Route
-          path="/payments"
-          element={
-            <RequireAuth>
-              <Payments />
-            </RequireAuth>
-          }
-        />
+        >
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/payments" element={<Payments />} />
+          <Route
+            path="/results"
+            element={
+              <RequirePayment>
+                <Results />
+              </RequirePayment>
+            }
+          />
+          <Route path="/admin" element={<Admin />} />
+        </Route>
 
       </Routes>
     </BrowserRouter>

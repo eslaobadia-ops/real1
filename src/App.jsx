faa0cadbb8daadc14108850e5b17bb897b869f62
eslaobadia-ops <eslaobadia@gmail.com>
2025-12-17 17:06:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Results from "./pages/Results";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import StudentDashboard from "./pages/StudentDashboard";

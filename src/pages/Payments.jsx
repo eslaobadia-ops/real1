65b8731b391085363402a21034789060fea1d4e3
eslaobadia-ops <eslaobadia@gmail.com>
2025-12-17 18:16:15 +0100

    Create Payments.jsx

---
+++ src/pages/Payments.jsx
@@ -0,0 +1,8 @@
+export default function Payments() {
+  return (
+    <div>
+      <h1>School Fees Payment</h1>
+      <p>Please complete payment to access results and CBT tests.</p>
+    </div>
+  );
+}

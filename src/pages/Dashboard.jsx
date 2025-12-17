746ec6e0fcd69fba6965eca6fa4d15cb938905c7
eslaobadia-ops <eslaobadia@gmail.com>
2025-12-17 09:01:42 +0100

    Create Dashboard.jsx

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Student Dashboard</h2>
+      <p>Welcome to Jaycrest Group of School</p>
+    </div>
+  );
+}
